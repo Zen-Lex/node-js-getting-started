e1f3b1024437ca4b4f46f1d8d3ff7ce60b54c214
Zenlex <jbertink@gmail.com>
2021-09-06 16:18:16 +0200

    deploy

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const face = require("cool-ascii-faces");
 const express = require('express')
+const app = express();
+
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/react", function (req, res) {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// app.use(express.static(path.join(__dirname, '/public')))
+// app.set('views', path.join(__dirname, 'views'))
+// app.set('view engine', 'ejs')
+
+// app.get('/react', (req, res) => {
+//     res.set("Content-Type", "text/html");
+//     res.sendFile(path.join(__dirname, "client/test-react/public/index.html"));
+// }); 
+
+app.get("/hello", (req, res) => {
+    res.send(face());
+}); 
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`)
+});
